405c2eaaf0a1c4c946b060b6f229c9c01b8f9c85
Greghori101 <e.souala@esi-sba.dz>
2025-11-30 22:20:44 +0100

    feat(database design): create models and migrations for user and address

---
+++ database/factories/AddressFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Address>
+ */
+class AddressFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+
+        return [
+            'country' => fake()->country(),
+            'city' => fake()->city(),
+            'post_code' => fake()->postcode(),
+            'street' => fake()->streetAddress(),
+            'created_at' => now(),
+        ];
+    }
+}
